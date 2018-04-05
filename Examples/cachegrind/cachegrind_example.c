4dfdc8ab1a001b903b4cd13fa82456b48000a3fb
Tobias Drewelies <tobias.drewelies@myestro.de>
2018-04-05 18:06:24 +0200

    mit neuen links

--- Examples/cachegrind/cachegrind_example.c
+++ Examples/cachegrind/cachegrind_example.c
@@ -1,17 +1,37 @@
 #include <stdlib.h>
+#include <stdio.h>
+
+void small(int r)
+{
+	int n = r*4;
+	printf("%d\n", n);
+}
+
+void big (int r)
+{
+	int n = r*2;
+	printf("%d\n", n);
+}
 
 int main(void)
 {
-    int i;
-
-    int n = 1000 * 1000;
+    int n = 3000 * 3000;
 
     int *block = malloc(n * sizeof(int));
 
-    for (i = 0; i < n / 10; i++) {
+    for (int i = 0; i < n / 10; i++) {
          int ri = rand() % n;
          block[ri] = 0;
     }
 
+	for (int i = 0; i<10; i++)
+	{
+		int r = rand();
+		if (r < (RAND_MAX/2))
+			small(r);
+		else
+			big(r);
+	}
+	free(block);
     return 0;
 }
