949ea73616ec85c9262d80979579c94826965875
Tobias Drewelies <tobias.drewelies@gmail.com>
2018-04-06 17:28:04 +0200

    Cachegrind ausarbeitung

--- Examples/cachegrind/cachegrind_example.c
+++ Examples/cachegrind/cachegrind_example.c
@@ -1,18 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void small(int r)
-{
-	int n = r*4;
-	printf("%d\n", n);
-}
-
-void big (int r)
-{
-	int n = r*2;
-	printf("%d\n", n);
-}
-
 int main(void)
 {
     int n = 1000 * 1000;
@@ -20,18 +8,10 @@ int main(void)
     int *block = malloc(n * sizeof(int));
 	int i;
     for (i = 0; i < n / 10; i++) {
-         //int ri = rand() % n;
-         block[(rand() % n)] = 0;
+         int ri = rand() % n;
+         block[ri] = ri;
     }
-
-	//for (i = 0; i<10; i++)
-	//{
-		//int r = rand()%10;
-		//if (r < 5)
-			//small(r);
-		//else
-			//big(r);
-	//}
+    
 	free(block);
     return 0;
 }
--- Examples/cachegrind/cachegrind_nomisses.c
+++ Examples/cachegrind/cachegrind_nomisses.c
@@ -1,18 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void small(int r)
-{
-	int n = r*4;
-	printf("%d\n", n);
-}
-
-void big (int r)
-{
-	int n = r*2;
-	printf("%d\n", n);
-}
-
 int main(void)
 {
     int n = 1000 * 1000;
@@ -24,14 +12,6 @@ int main(void)
          block[i] = ri;
     }
 
-	//for (i = 0; i<10; i++)
-	//{
-		//int r = rand()%10;
-		//if (i%2)
-			//small(r);
-		//else
-			//big(r);
-	//}
 	free(block);
     return 0;
 }
